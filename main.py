8207c7add575f134dc41db3e9aa570e1f800975e
andresrokp <afrokap@gmail.com>
2022-07-11 11:32:33 -0500

    querying each PM in the IPs list

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ import myvars as mv
 
 print("-\n--\n---\n----\n-----\n------")
 
-mp.hello(mv.x)
+from pymodbus.client.sync import ModbusTcpClient
+
+for idx, ipAdrs in enumerate(mv.ipAddressList):
+    client = ModbusTcpClient(ipAdrs)
+    client.connect()
+    # registers are addressed starting from zero
+    result = client.read_holding_registers(2699, 2, unit=0x00)
+    print(result.registers)
 
 print("---\n--\n-")
